4a12b3fb380673241e86592d17fde2db94ec0e81
LIGHTS\k.harris <k.harris@digipen.edu>
2016-10-07 10:33:29 -0700

    time stop sfx added

---
+++ Space-Time/Assets/Scripts/TimeStopFX.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeStopFX : MonoBehaviour {
+
+    CameraController CameraScript;
+    bool timeStopped;
+    bool timeFXplayed = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        CameraScript = GameObject.Find("Main Camera").GetComponent<CameraController>();
+        timeStopped = CameraScript.GetPTime();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timeStopped = CameraScript.GetPTime();
+
+        if (!timeStopped)
+            timeFXplayed = true;
+
+
+        if (timeStopped && timeFXplayed)
+        {
+            AkSoundEngine.PostEvent("timeStopSound", this.gameObject);
+            timeFXplayed = false;
+        }
+	}
+}
